527531c8d23bced5c329bbd9aa905fd893ed6a2b
Glitch (imagesearchabstractionlayer1) <none>
2018-03-12 04:12:18 +0000

    🖼🐗 Checkpoint README.md:471758/31 package.json:471758/43 server.js:471758/261

--- server.js
+++ server.js
@@ -1,37 +1,14 @@
-// server.js
-// where your node app starts
 
-// init project
 var express = require('express');
 var app = express();
+var bodyParser= require('body-parser');
+var cors= require('cors');
+var mongoose = require('mongoose');
+var Big = require('node-bing-api')({accKey: ' '});
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+app.use(bodyParser.json());
+app.use(cors());
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
