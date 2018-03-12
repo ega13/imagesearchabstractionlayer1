eed84fb7002d4ed1e5d442710eaaeb54a54373c1
Glitch (imagesearchabstractionlayer1) <none>
2018-03-12 04:22:49 +0000

    🛣👓 Checkpoint server.js:471758/228

--- server.js
+++ server.js
@@ -4,10 +4,20 @@ var app = express();
 var bodyParser= require('body-parser');
 var cors= require('cors');
 var mongoose = require('mongoose');
-var Big = require('node-bing-api')({accKey: ' '});
+var Big = require('node-bing-api')({accKey: ' 5dd324c7c56247f8afaa711de888b14c'});
 
 app.use(bodyParser.json());
 app.use(cors());
+
+app.get('/api/imagesearch/: searchVal*' , (res,req,next) =>{
+var {searchVal} = req.params;
+  var {offset} = req.query;
+  
+  res.json({
+  searchVal,
+  offset
+  });
+});
 
 
 // listen for requests :)
