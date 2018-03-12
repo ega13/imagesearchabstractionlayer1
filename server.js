fb8d5fbd2824b1a2f59e58e3f027368b808bc735
Glitch (imagesearchabstractionlayer1) <none>
2018-03-12 05:04:48 +0000

    😀👧 Checkpoint server.js:471758/185

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ var app = express();
 var bodyParser= require('body-parser');
 var cors= require('cors');
 var mongoose = require('mongoose');
-var Big = require('node-bing-api')({accKey: ' 5dd324c7c56247f8afaa711de888b14c'});
+var Bing = require('node-bing-api')({accKey: ' 5dd324c7c56247f8afaa711de888b14c'});
 var searchTerm = require('./models/searchterm');
 
 app.use(bodyParser.json());
 app.use(cors());
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/searchTerm');
+
+app.get('/api/recentsearchs', (res,req,next) =>{
+searchTerm.find({},(err,data) =>{
+res.json(data);
+});
+});
 
 app.get('/api/imagesearch/: searchVal*' , (res,req,next) =>{
 var {searchVal} = req.params;
@@ -23,13 +29,9 @@ var {searchVal} = req.params;
     if(err){
     res.send('error saving to database');
     }
-    res.json(data);
+    
   });
-  
-  return res.json({
-  searchVal,
-  offset
-  });
+  Bing.images
 });
 
 
