09a08c6f6845de76b1d790805d654f53603763ad
Glitch (imagesearchabstractionlayer1) <none>
2018-03-18 20:33:54 +0000

    👬🍜 Checkpoint ./server.js:471758/128

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var bodyParser= require('body-parser');
 var cors= require('cors');
 var mongoose = require('mongoose');
-var Bing = require('node-bing-api')({accKey: ' 5dd324c7c56247f8afaa711de888b14c'});
+var Bing = require('node-bing-api')({accKey: ' 3029f79b8d3844438b296549c0daabb1'});
 var searchTerm = require('./models/searchterm');
 
 app.use(bodyParser.json());
